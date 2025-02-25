e520a72edfee27db69914354b4396634f6dbc2cd
Nayan Thacker <157197883+Nayan1729@users.noreply.github.com>
2025-02-25 15:32:43 +0530

    Time: 4 ms (64.21%), Space: 16.7 MB (6.91%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
+    stack<int> emptyStack(stack<int>s){
+        while(!s.empty()){
+            s.pop();
+        }
+        return s;
+    }
     long long subArrayRanges(vector<int>& a) {
-        int n = a.size() , sum=0; 
+        // O(n^2)
+        /* int n = a.size() ;
+        long long sum=0; 
         for(int i=0;i<n;i++){
             int maxx = a[i] , minn  = a[i];
             for(int j = i ; j < n ;j++){
@@ -11,5 +19,83 @@ public:
             }
         }
         return sum;
+        */
+        int n = a.size();
+        stack<int>s;
+        vector<int>prevSmall(n,-1),nextSmall(n,-1),prevGreat(n,-1) , nextGreat(n,-1);
+
+        //prev Smaller find
+        for(int i=0;i<n;i++){
+            while(!s.empty() && a[i] <= a[s.top()]){
+                s.pop();
+            }
+            if(!s.empty()){
+                prevSmall[i] = s.top();
+            }
+            s.push(i);
+        }
+        s = emptyStack(s);
+        // prev Greater find
+        for(int i=0;i<n;i++){
+            while(!s.empty() && a[i] >= a[s.top()]){
+                s.pop();
+            }
+            if(!s.empty()){
+                prevGreat[i] = s.top();
+            }
+            s.push(i);
+        }
+        s = emptyStack(s);
+
+         // next Smaller find use < only
+         for(int i = n-1 ; i >= 0 ; i--){
+            while(!s.empty() && a[i] < a[s.top()]){
+                s.pop();
+            }
+            if(!s.empty()){
+                nextSmall[i] = s.top();
+            }
+            s.push(i);
+        }
+        s = emptyStack(s);
+        for(int i = n-1 ; i >= 0 ; i--){
+            while(!s.empty() && a[i] > a[s.top()]){
+                s.pop();
+            }
+            if(!s.empty()){
+                nextGreat[i] = s.top();
+            }
+            s.push(i);
+        }
+        long long maxContriubtionsSum = 0 , minContriubtionsSum = 0;
+        long long lowerSmall , upperSmall , lowerGreat , upperGreat;
+        for(int i = 0; i < n ; i++ ){
+            if(prevSmall[i] == -1){
+                lowerSmall = 1 + i ;
+            }else{
+                lowerSmall = i - prevSmall[i];
+            }
+
+            if(nextSmall[i] == -1){
+                upperSmall = n-i ;
+            }else{
+                upperSmall =  nextSmall[i] - i ;
+            }
+
+            if(prevGreat[i] == -1){
+                lowerGreat = 1 + i ;
+            }else{
+                lowerGreat = i - prevGreat[i];
+            }
+
+            if(nextGreat[i] == -1){
+                upperGreat = n - i ;
+            }else{
+                upperGreat =  nextGreat[i] - i ;
+            }
+            maxContriubtionsSum += (long long)upperGreat * lowerGreat * a[i] ;
+            minContriubtionsSum += (long long)upperSmall * lowerSmall * a[i] ;
+        }
+        return maxContriubtionsSum - minContriubtionsSum;
     }
 };
