0c46b5de6e50150b945ef284c814f1c726e47eb7
Nayan Thacker <157197883+Nayan1729@users.noreply.github.com>
2025-03-08 15:56:20 +0530

    Time: 7 ms (69.91%), Space: 11 MB (57.6%) - LeetHub

---
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfSubstrings(string a) {
+        int freq[3] = {0}; // Frequency array for 'a', 'b', and 'c'
+        int r = 0, l = 0, n = a.length(), total = 0;
+
+        while (r < n) {
+            freq[a[r] - 'a']++; // Increment frequency of current character
+            while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) { 
+                total += (n - r); // Every valid substring contributes
+                freq[a[l] - 'a']--; // Shrink the window from the left
+                l++;
+            }
+            r++;
+        }
+        return total;
+    }
+};
