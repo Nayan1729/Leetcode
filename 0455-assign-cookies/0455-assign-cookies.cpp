8e1aa9048bb51e8b2d7bb2e2043c991acae7d95c
Nayan Thacker <157197883+Nayan1729@users.noreply.github.com>
2025-03-22 23:12:06 +0530

    Time: 9 ms (53.7%), Space: 44.8 MB (74.78%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& c) {
-        int i , j , count = 0;
+        // int i , j , count = 0;
+        // sort(g.begin() , g.end() );
+        // sort(c.begin() , c.end() );
+        // vector<int> assigned(c.size() , 0);
+
+        // for( i=0 ; i< g.size() ; i++){
+        //     for(j=0; j< c.size();j++ ){
+        //         if(!assigned[j] && c[j] >= g[i]){
+        //             count++;
+        //             assigned[j] = 1;
+        //             break;
+        //         }
+        //     }
+        //      if( j == c.size() ){
+        //             return count;
+        //         }
+        // }
+
+        // Solution 2 more optimized
+        int greedIndex = 0 , cookieIndex = 0 , count =0; 
         sort(g.begin() , g.end() );
         sort(c.begin() , c.end() );
-        vector<int> assigned(c.size() , 0);
 
-        for( i=0 ; i< g.size() ; i++){
-            for(j=0; j< c.size();j++ ){
-                if(!assigned[j] && c[j] >= g[i]){
-                    count++;
-                    assigned[j] = 1;
-                    break;
-                }
+        while(greedIndex < g.size() && cookieIndex < c.size() ){
+            if(g[greedIndex] <= c[cookieIndex] ){
+                count++;
+                greedIndex++;
             }
-             if( j == c.size() ){
-                    return count;
-                }
+            cookieIndex++;
         }
         return count;
     }
