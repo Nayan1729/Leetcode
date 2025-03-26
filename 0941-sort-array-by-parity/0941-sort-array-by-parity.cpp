49d9c28270ed528bc8d55f97bc5ed257c0269e2c
Nayan Thacker <157197883+Nayan1729@users.noreply.github.com>
2025-03-26 12:20:28 +0530

    Time: 0 ms (100%), Space: 20.5 MB (68.56%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int n = nums.size() , i, j=0;
-        vector<int>  a;
-        for(i=0 ; i<n ; i++){
-            if(nums[i]%2 == 0){
-                a.push_back(nums[i]);
+        // int n = nums.size() , i, j=0;
+        // vector<int>  a;
+        // for(i=0 ; i<n ; i++){
+        //     if(nums[i]%2 == 0){
+        //         a.push_back(nums[i]);
+        //     }
+        // }
+        // for(i=0 ; i<n ; i++){
+        //     if(nums[i]%2 == 1){
+        //         a.push_back(nums[i]);
+        //     }
+        // }
+        // return a;
+        int n = nums.size();
+        int i = 0, j = n-1 ,evenIndex = 0 , oddIndex = n-1;
+        vector<int> a(n,0);
+        while(i!=n){
+            if(nums[i] % 2 == 0 ){ // even Condition
+                a[evenIndex] = nums[i];
+                evenIndex++;
             }
-        }
-        for(i=0 ; i<n ; i++){
-            if(nums[i]%2 == 1){
-                a.push_back(nums[i]);
+            if(nums[j] % 2 == 1 ){ // Odd condition
+                a[oddIndex] = nums[j];
+                oddIndex--;
             }
+            i++;
+            j--;
         }
         return a;
     }
