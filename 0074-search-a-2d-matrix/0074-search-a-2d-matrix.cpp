b760932372b959e13b9eb38a64aef71df0ed02f9
Nayan Thacker <157197883+Nayan1729@users.noreply.github.com>
2025-04-01 16:21:09 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.46%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& a, int k) {
-        int i = 0 , n = a.size() , m = a[0].size() ,l = 0 ,r = m - 1;
-        for(;i < n ; i++ ){
-            if(k < a[i][l]){
-                return false;
-            }
-            if( !(k >= a[i][l] && k <= a[i][r])){
-                continue;
-            }
-            // Binary search to find the array
-            while(l <= r){
+        // int i = 0 , n = a.size() , m = a[0].size() ,l = 0 ,r = m - 1;
+        // for(;i < n ; i++ ){
+        //     if(k < a[i][l]){
+        //         return false;
+        //     }
+        //     if( !(k >= a[i][l] && k <= a[i][r])){
+        //         continue;
+        //     }
+        //     // Binary search to find the array
+        //     while(l <= r){
+        //         int mid = (l + r)/2;
+        //         if(k == a[i][mid]){
+        //             return true;
+        //         }else if(k > a[i][mid]){
+        //             l = mid + 1;
+        //         }else{
+        //             r = mid - 1;
+        //         }
+        //     }
+        // }
+        // return false;
+        // Imagine like a 1D array
+         int i = 0 , n = a.size() , m = a[0].size() ,l = 0 , r = m *n - 1 , row , col;
+         while(l <= r){
                 int mid = (l + r)/2;
-                if(k == a[i][mid]){
+                 row = mid / m ;
+                 col = mid % m;
+                if(k == a[row][col]){
                     return true;
-                }else if(k > a[i][mid]){
+                }else if(k > a[row][col]){
                     l = mid + 1;
                 }else{
                     r = mid - 1;
                 }
             }
-        }
-        return false;
+            return false;
     }
 };
