fccb0c99b40f463b1f3d5a7e0a19e53a1d17e8ea
Nayan Thacker <157197883+Nayan1729@users.noreply.github.com>
2025-04-01 16:52:53 +0530

    Time: 0 ms (100%), Space: 10.3 MB (34.95%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& a) {
-        int i,j,k,n=a.size();
-        for(i=0;i<n;i++){
-            for(j=i;j<a[0].size();j++){
+        int i,j,k,m=a.size() , n = a[0].size() ;
+        for(i=0;i<m;i++){
+            for(j=i;j<n;j++){
                 swap(a[i][j],a[j][i]);
             }
         }
-        for(j=0;j<(a[0].size())/2;j++){
+        for(j=0;j<n/2;j++){
             for(i=0;i<a.size();i++){
-                swap(a[i][j],a[i][n-1-j]);
+                swap(a[i][j],a[i][m-1-j]);
             }
         }
     }
