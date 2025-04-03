a427340e714dc840175f9e82045e481fa268e63a
Nayan Thacker <157197883+Nayan1729@users.noreply.github.com>
2025-04-03 09:48:41 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.88%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,29 +1,68 @@
 class Solution {
 public: 
-    int mini = INT_MAX , maxi = -1;
-    void bs(vector<int> a , int l , int h , int k ){
-        if(l > h ){
-            return;
+    // int mini = INT_MAX , maxi = -1;
+    // void bs(vector<int> a , int l , int h , int k ){
+    //     if(l > h ){
+    //         return;
+    //     }
+    //     int mid = (l + h )/2;
+    //     if( k == a[mid] ){
+    //         mini = min(mid , mini);
+    //         maxi = max(mid , maxi);
+    //         bs(a , l , mid - 1 , k ); // Move to the left
+    //         bs( a , mid + 1 , h , k );   // Move to the right 
+    //         return ;
+    //     }else if(k > a[mid]){
+    //         bs( a , mid + 1 , h , k );
+    //         return ; 
+    //     }else{
+    //         bs(a , l , mid - 1 , k );
+    //         return ; 
+    //     }
+    // }
+    //   vector<int> searchRange(vector<int>& a, int k) {
+    //     int n = a.size() ;
+    //     bs(a , 0 , n - 1 , k);
+    //     mini = mini == INT_MAX ? -1 : mini;
+    //     return {mini , maxi};
+    // }
+
+    // Optimised
+    
+    
+    int firstOccurrence(vector<int>& a, int k) {
+        int l = 0, h = a.size() - 1, first = -1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (a[mid] == k) {
+                first = mid;
+                h = mid - 1; // Move left to find earlier occurrence
+            } else if (a[mid] < k) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
         }
-        int mid = (l + h )/2;
-        if( k == a[mid] ){
-            mini = min(mid , mini);
-            maxi = max(mid , maxi);
-            bs(a , l , mid - 1 , k ); // Move to the left
-            bs( a , mid + 1 , h , k );   // Move to the right 
-            return ;
-        }else if(k > a[mid]){
-            bs( a , mid + 1 , h , k );
-            return ; 
-        }else{
-            bs(a , l , mid - 1 , k );
-            return ; 
-        }
+        return first;
     }
-      vector<int> searchRange(vector<int>& a, int k) {
-        int n = a.size() ;
-        bs(a , 0 , n - 1 , k);
-        mini = mini == INT_MAX ? -1 : mini;
-        return {mini , maxi};
+    
+    int lastOccurrence(vector<int>& a, int k) {
+        int l = 0, h = a.size() - 1, last = -1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (a[mid] == k) {
+                last = mid;
+                l = mid + 1; // Move right to find later occurrence
+            } else if (a[mid] < k) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+        return last;
+    }
+    
+    vector<int> searchRange(vector<int>& a, int k) {
+        return {firstOccurrence(a, k), lastOccurrence(a, k)};
     }
 };
