91f58658e511f8ef3de5ab9dfdef3f8793e2b843
Nayan Thacker <157197883+Nayan1729@users.noreply.github.com>
2025-05-14 18:28:30 +0530

    Time: 0 ms (100%), Space: 14.9 MB (16.55%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,17 +1,37 @@
+// class Solution {
+// public:
+//     vector<int> intersect(vector<int>& a, vector<int>& b) {
+//         vector<int> res;
+//         int i , j , m = a.size() , n = b.size();
+//         for(i=0; i < m ;i++ ){
+//             for(j=0;j<n;j++){
+//                 if(a[i]==b[j]){
+//                     res.push_back(a[i]);
+//                     b[j] = -1; //  SO that it doesnt repeat again
+//                     break;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
 class Solution {
 public:
-    vector<int> intersect(vector<int>& a, vector<int>& b) {
-        vector<int> res;
-        int i , j , m = a.size() , n = b.size();
-        for(i=0; i < m ;i++ ){
-            for(j=0;j<n;j++){
-                if(a[i]==b[j]){
-                    res.push_back(a[i]);
-                    b[j]=-1; //  SO that it doesnt repeat again
-                    break;
-                }
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> freq;
+        vector<int> result;
+
+        for (int num : nums1) {
+            freq[num]++;
+        }
+
+        for (int num : nums2) {
+            if (freq[num] > 0) {
+                result.push_back(num);
+                freq[num]--;
             }
         }
-        return res;
+
+        return result;
     }
 };
