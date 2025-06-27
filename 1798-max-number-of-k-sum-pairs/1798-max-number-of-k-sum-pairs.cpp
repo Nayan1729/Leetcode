edd230b7cfed0a9655ff2022bce787475fbc1bc9
Nayan Thacker <157197883+Nayan1729@users.noreply.github.com>
2025-06-27 15:52:29 +0530

    Time: 90 ms (67.37%), Space: 62 MB (52.67%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int maxOperations(vector<int>& a, int k) {
-        unordered_map<int , int> mp;
-        int i , count = 0 , n = a.size() ;
-        for( i = 0 ; i < n ; i++){
-            int curr = a[i];
-            if( mp.find(k - curr) != mp.end() && mp[k-curr] > 0  ){
-                mp[k - curr]--;
+        // unordered_map<int , int> mp;
+        // int i , count = 0 , n = a.size() ;
+        // for( i = 0 ; i < n ; i++){
+        //     int curr = a[i];
+        //     // If u find the better half and its count is also more than 0
+        //     if( mp.find(k - curr) != mp.end() && mp[k-curr] > 0 ){ 
+        //         mp[k - curr]--;
+        //         count++;       
+        //     }// If u cant find then increase the count 
+        //     else{
+        //             mp[curr]++;
+        //     }
+        // }
+        // return count;
+
+        int left = 0 , n = a.size() , right = n - 1 , count = 0 ;
+        sort(a.begin(),a.end());
+        while ( left < right ){
+            if(a[left]+ a[right] == k ){
+                left++;
+                right--;
                 count++;
+            }else if(a[left]+ a[right] > k ){
+                right--;
             }else{
-                if(mp.find(curr) != mp.end() ){
-                    mp[curr]++;
-                }else{
-                    mp[curr] = 1;
-                }
+                left++;
             }
         }
         return count;
